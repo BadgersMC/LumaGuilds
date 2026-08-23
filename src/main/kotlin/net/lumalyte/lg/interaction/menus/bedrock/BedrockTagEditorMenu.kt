d5f5d08cc2e29ee97b7a99dd49b68d7f3f039e0d
BadgersMC <blrusso18@gmail.com>
2026-08-22 21:36:48 -0400

    test(i18n): prove bedrock validation output

--- src/main/kotlin/net/lumalyte/lg/interaction/menus/bedrock/BedrockTagEditorMenu.kt
+++ src/main/kotlin/net/lumalyte/lg/interaction/menus/bedrock/BedrockTagEditorMenu.kt
@@ -133,8 +133,7 @@ class BedrockTagEditorMenu(
 
                         // Reject interactive MiniMessage event tags (click/hover/insertion)
                         net.lumalyte.lg.utils.GuildTagValidator.validationFailure(value, configService.loadConfig().guild.nameFilter)?.let {
-                            val reason = GuildTagValidationMessages.legacy(lang, it)
-                            return@getValidator ValidationResult.invalid(reason)
+                            return@getValidator GuildTagValidationMessages.invalid(lang, it)
                         }
 
                         // Allow both § codes (Bedrock) and MiniMessage format
